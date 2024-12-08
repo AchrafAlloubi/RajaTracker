458b9bae0c883c784d04fd7b12cd7cd35c5942a9
Achraf Alloubi <achraf.alloubi@saaq.gouv.qc.ca>
2024-12-08 00:52:13 -0500

    Initial commit

---
+++ backend/src/main/java/com/example/backend_raja_tracker/BackendRajaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_raja_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRajaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRajaTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/backend_raja_tracker/BackendRajaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_raja_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRajaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
